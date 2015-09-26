1782594dfce6af72412cf4c2dcb46dbe14282a2e
aleju <kontakt@ajung.name>
2015-09-26 19:26:06 +0200

    Add code to train V

--- train_v.lua
+++ train_v.lua
@@ -1,6 +1,8 @@
 require 'torch'
 require 'image'
 require 'paths'
+require 'pl' -- this is somehow responsible for lapp working in qlua mode
+require 'optim'
 ok, DISP = pcall(require, 'display')
 if not ok then print('display not found. unable to plot') end
 DATASET = require 'dataset'
@@ -26,6 +28,7 @@ OPT = lapp[[
 if OPT.gpu < 0 or OPT.gpu > 3 then OPT.gpu = false end
 print(OPT)
 
+math.randomseed(OPT.seed)
 torch.manualSeed(OPT.seed)
 torch.setnumthreads(OPT.threads)
 
@@ -53,6 +56,7 @@ else
     require 'nn'
 end
 require 'dpnn'
+require 'LeakyReLU'
 torch.setdefaulttensortype('torch.FloatTensor')
 
 
@@ -74,7 +78,7 @@ function main()
     end
     ----------------------------------------------------------------------
     
-    local V = create_V()
+    V = create_V()
     
     if OPT.gpu then
         V:cuda()
@@ -92,12 +96,14 @@ function main()
     CRITERION = nn.BCECriterion()
     PARAMETERS_V, GRAD_PARAMETERS_V = V:getParameters()
     CONFUSION = optim.ConfusionMatrix(CLASSES)
-    OPTSTATE = {adam:{}}
+    OPTSTATE = {adam={}}
     
     EPOCH = 1
     while true do
         TRAIN_DATA = DATASET.loadRandomImages(OPT.N_epoch)
+        print(string.format("<trainer> Epoch %d", EPOCH))
         epoch(V)
+        visualizeProgress()
     end
 end
 
@@ -105,6 +111,7 @@ function create_V()
     local model = nn.Sequential()
     local activation = nn.LeakyReLU
     
+    --[[
     model:add(nn.SpatialConvolution(IMG_DIMENSIONS[1], 32, 3, 3, 1, 1, (3-1)/2))
     model:add(activation())
     model:add(nn.SpatialConvolution(32, 32, 3, 3, 1, 1, (3-1)/2))
@@ -122,7 +129,7 @@ function create_V()
     model:add(nn.View(64, 16 * 16))
   
     local parallel = nn.Parallel(2, 2)
-    for i=1,128 do
+    for i=1,64 do
         local lin = nn.Sequential()
         lin:add(nn.Linear(16*16, 128))
         lin:add(activation())
@@ -133,9 +140,9 @@ function create_V()
         parallel:add(lin)
     end
     model:add(parallel)
-    model:add(nn.BatchNormalization(128*8))
+    model:add(nn.BatchNormalization(64*8))
   
-    model:add(nn.Linear(128*8, 128))
+    model:add(nn.Linear(64*8, 128))
     model:add(activation())
     model:add(nn.BatchNormalization(128))
     model:add(nn.Dropout())
@@ -146,7 +153,39 @@ function create_V()
     model:add(nn.Dropout())
   
     model:add(nn.Linear(128, 2))
-    model:add(nn.Softmax())
+    model:add(nn.SoftMax())
+    --]]
+  
+    model:add(nn.SpatialConvolution(IMG_DIMENSIONS[1], 64, 3, 3, 1, 1, (3-1)/2))
+    model:add(activation())
+    model:add(nn.SpatialConvolution(64, 64, 3, 3, 1, 1, (3-1)/2))
+    model:add(activation())
+    model:add(nn.SpatialMaxPooling(2, 2))
+    model:add(nn.SpatialBatchNormalization(64))
+    model:add(nn.Dropout())
+  
+    model:add(nn.SpatialConvolution(64, 128, 3, 3, 1, 1, (3-1)/2))
+    model:add(activation())
+    model:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, (3-1)/2))
+    model:add(activation())
+    model:add(nn.SpatialMaxPooling(2, 2))
+    model:add(nn.SpatialBatchNormalization(128))
+    model:add(nn.SpatialDropout())
+    model:add(nn.View(128 * 8 * 8))
+    model:add(nn.BatchNormalization(128*8*8))
+  
+    model:add(nn.Linear(128*8*8, 1024))
+    model:add(activation())
+    model:add(nn.BatchNormalization(1024))
+    model:add(nn.Dropout())
+  
+    model:add(nn.Linear(1024, 1024))
+    model:add(activation())
+    model:add(nn.BatchNormalization(1024))
+    model:add(nn.Dropout())
+  
+    model:add(nn.Linear(1024, 2))
+    model:add(nn.SoftMax())
   
     model = require('weight-init')(model, 'heuristic')
   
@@ -158,7 +197,7 @@ function epoch(model)
     local batchIdx = 0
     local trained = 0
     while trained < OPT.N_epoch do
-        local thisBatchSize = math.min(OPT.batchSize, N_epoch - t + 1)
+        local thisBatchSize = math.min(OPT.batchSize, OPT.N_epoch - trained)
         local inputs = torch.Tensor(thisBatchSize, IMG_DIMENSIONS[1], IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
         local targets = torch.Tensor(thisBatchSize, 2)
         
@@ -186,9 +225,11 @@ function epoch(model)
 
             -- update confusion (add 1 since targets are binary)
             for i = 1,thisBatchSize do
-                local c
-                if outputs[i][1] > 0.5 then c = 1 else c = 2 end
-                CONFUSION:add(c, targets[i][1]+1)
+                local predictedClass
+                local realClass
+                if outputs[i][1] > 0.5 then predictedClass = 0 else predictedClass = 1 end
+                if targets[i][1] == 1 then realClass = 0 else realClass = 1 end
+                CONFUSION:add(predictedClass+1, realClass+1)
             end
 
             -- Clamp V's gradients
@@ -228,12 +269,12 @@ function epoch(model)
         trained = trained + thisBatchSize
         batchIdx = batchIdx + 1
         
-        xlua.progress(trained, N_epoch)
+        xlua.progress(trained, OPT.N_epoch)
     end
     
     local epochTime = sys.clock() - startTime
     print(string.format("<trainer> time required for this epoch = %d s", epochTime))
-    print(string.format("<trainer> time to learn 1 sample = %f ms", 1000 * epochTime/N_epoch))
+    print(string.format("<trainer> time to learn 1 sample = %f ms", 1000 * epochTime/OPT.N_epoch))
     print("Confusion of V:")
     print(CONFUSION)
     CONFUSION:zero()
@@ -252,37 +293,408 @@ function epoch(model)
     EPOCH = EPOCH + 1
 end
 
-function createSyntheticImages(N)
+function visualizeProgress()
+    -- deactivate dropout
+    V:evaluate()
+    
+    local imagesReal = DATASET.loadRandomImages(50)
+    local imagesFake = createSyntheticImages(50)
+    local both = torch.Tensor(100, IMG_DIMENSIONS[1], IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
+    for i=1,imagesReal:size(1) do
+        both[i] = imagesReal[i]
+    end
+    for i=1,#imagesFake do
+        both[imagesReal:size(1) + i] = imagesFake[i]
+    end
+    
+    local predictions = V:forward(both)
+    local goodImages = {}
+    local badImages = {}
+    for i=1,predictions:size(1) do
+        if torch.any(both[i]:gt(1.0)) then
+            print("[WARNING] bad values in image")
+            print(both[i][both[i]:gt(1.0)])
+            print("image i=", i, " is ge1")
+        end
+        if torch.any(both[i]:lt(0.0)) then
+            print("[WARNING] bad values in image")
+            print(both[i][both[i]:lt(0.0)])
+            print("image i=", i, " is lt0")
+        end
+    
+        --print("i=", i, "predictions[i][1]=", predictions[i][1])
+        if predictions[i][1] < 0.5 then
+            goodImages[#goodImages+1] = both[i]
+        else
+            badImages[#badImages+1] = both[i]
+        end
+    end
+    
+    if #goodImages > 0 then
+        DISP.image(goodImages, {win=OPT.window+0, width=IMG_DIMENSIONS[3]*15, title="V: rated as real images (Epoch " .. EPOCH .. ")"})
+    end
+    if #badImages > 0 then
+        DISP.image(badImages, {win=OPT.window+1, width=IMG_DIMENSIONS[3]*15, title="V: rated as fake images (EPOCH " .. EPOCH .. ")"})
+    end
+    
+    -- reactivate dropout
+    V:training()
+end
+
+function createSyntheticImages(N, allowSubcalls)
+    if allowSubcalls == nil then allowSubcalls = true end
+    local images
+    local p = math.random()
+    if p < 1/4 then
+        --print("mix")
+        images = createSyntheticImagesMix(N)
+    elseif p >= 1/4 and p < 2/4 then
+        --print("warp")
+        images = createSyntheticImagesWarp(N)
+    elseif p >= 2/4 and p < 3/4 then
+        --print("stamp")
+        images = createSyntheticImagesStamp(N)
+    else
+        --print("random")
+        images = createSyntheticImagesRandom(N)
+    end
+    
+    
+    if allowSubcalls and math.random() < 0.33 then
+        --print("sub")
+        local otherImages = createSyntheticImages(N, false)
+        images = mixImageLists(images, otherImages)
+    end
+    --print("done")
+    
+    --for i=1,#images do
+    --    image.display(images[i])
+    --    io.read()
+    --end
+    
+    return images
+end
+
+function mixImages(img1, img2, overlay)
+    local img = torch.Tensor(IMG_DIMENSIONS[1], IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
+    img:zero()
+    
+    if overlay == nil then
+        if math.random() < 0.5 then
+            --overlay = createGaussianOverlay(img1:size(2), img1:size(3), 400+math.random(900))
+            overlay = getGaussianOverlay()
+        else
+            overlay = createPixelwiseOverlay(IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
+        end
+    end
+    
+    overlay = torch.repeatTensor(overlay, 3, 1, 1)
+    --    overlay * img1     + (1 - overlay) * img2
+    img = overlay:clone():cmul(img1) + overlay:clone():mul(-1):add(1):cmul(img2)
+    img:div(torch.max(img))
+    
+    --image.display(img1)
+    --image.display(img2)
+    --image.display(overlay)
+    --image.display(img)
+    --io.read()
+    
+    --[[
+    for y=1,img1:size(2) do
+        for x=1,img1:size(3) do
+            for c=1,img1:size(1) do
+                img[c][y][x] = overlay[y][x] * img1[c][y][x] + (1 - overlay[y][x]) * img2[c][y][x]
+            end
+        end
+    end
+    --]]
+    
+    return img
+end
+
+function mixImageLists(images1, images2)
     local images = {}
+    local overlay
+    local p = math.random()
+    if p < 0.5 then
+        --overlay = createGaussianOverlay(IMG_DIMENSIONS[2], IMG_DIMENSIONS[3], 400+math.random(900))
+        overlay = getGaussianOverlay()
+    else
+        overlay = createPixelwiseOverlay(IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
+    end
+    
+    for i=1,#images1 do
+        images[i] = mixImages(images1[i]:clone(), images2[i]:clone(), overlay)
+    end
+    
+    return images
+end
+
+function createSyntheticImagesMix(N)
+    local images = {}
+    local img1 = {}
+    local img2 = {}
     
     for i=1,N do
-        local p = 0.5
+        table.insert(img1, TRAIN_DATA[math.random(TRAIN_DATA:size())])
+        table.insert(img2, TRAIN_DATA[math.random(TRAIN_DATA:size())])
+    end
+    
+    return mixImageLists(img1, img2)
+end
+
+function createSyntheticImagesStamp(N)
+    local images = {}
+    
+    local nbChannels = TRAIN_DATA[1]:size(1)
+    local maxY = TRAIN_DATA[1]:size(2)
+    local maxX = TRAIN_DATA[1]:size(3)
+    
+    --local overlay = createGaussianOverlay(maxY, maxX, 1000+math.random(1000))
+    local overlay = getGaussianOverlay()
+    
+    for i=1,N do
+        local p = math.random()
+        local img1 = TRAIN_DATA[math.random(TRAIN_DATA:size())]
+        local img = torch.Tensor(nbChannels, maxY, maxX)
+        img:zero()
+        local direction = {math.random(10), math.random(10)}
+        
+        for y=1,maxY do
+            for x=1,maxX do
+                local coords = withinImageCoords(y + direction[1], x + direction[2], img:size(2), img:size(3))
+                
+                for c=1,nbChannels do
+                    local usualVal = img1[c][y][x]
+                    local sourceVal = img1[c][coords[1]][coords[2]]
+                    --print("overlay:", overlay[y][x], "usualval:", usualVal, "sourceval:", sourceVal, "result:", (1 - overlay[y][x]) * usualVal + overlay[y][x] * sourceVal)
+                    img[c][y][x] = (1 - overlay[y][x]) * usualVal + overlay[y][x] * sourceVal
+                end
+            end
+        end
+        img:div(torch.max(img))
+        
+        table.insert(images, img)
+    end
+    
+    return images
+end
+
+function withinImageCoords(y, x, maxY, maxX)
+    y = y % maxY
+    if y < 1 then
+        y = y * (-1)
+        y = maxY - y
+    end
+    
+    x = x % maxX
+    if x < 1 then
+        x = x * (-1)
+        x = maxX - x
+    end
+    
+    return {y, x}
+end
+
+function createSyntheticImagesWarp(N)
+    local images = {}
+    
+    --local overlay1 = createGaussianOverlay(OPT.scale, OPT.scale)
+    --local overlay2 = createGaussianOverlay(OPT.scale, OPT.scale)
+    local overlay1 = getGaussianOverlay()
+    local overlay2 = getGaussianOverlay()
+    overlay1:mul(2.0)
+    overlay1:add(-1.0)
+    overlay2:mul(2.0)
+    overlay2:add(-1.0)
+    
+    for i=1,N do
         local img1 = TRAIN_DATA[math.random(TRAIN_DATA:size())]:clone()
-        local img2 = TRAIN_DATA[math.random(TRAIN_DATA:size())]:clone()
-        local image = torch.Tensor(img1:size())
-        image:zero()
+        local flow = torch.Tensor(2, img1:size(2), img1:size(3))
+        flow:zero()
+        
+        local direction = {1, 0}
+        local length = 1 + math.random(4.0)
+        --local length = 3.0
         
         for y=1,img1:size(2) do
             for x=1,img1:size(3) do
-                -- loop over channel
-                for c=1,img1:size(1) do
-                    if math.random() > p then
-                        image[c][y][x] = img1[c][y][x]
-                    end
-                    
-                    if math.random() > 0.5 then
-                        p = math.max(p - 0.05, 0)
-                    else
-                        p = math.min(p + 0.05, 1.0)
-                    end
+                flow[1][y][x] = overlay1[y][x] * length
+                flow[2][y][x] = overlay2[y][x] * length
+            end
+        end
+        
+        local img = image.warp(img1, flow)
+        img:div(torch.max(img))
+        
+        table.insert(images, img)
+    end
+    
+    return images
+end
+
+function createSyntheticImagesRandom(N)
+    local images = {}
+    --local overlay1 = createGaussianOverlay(OPT.scale, OPT.scale, 2000, 10)
+    --local overlay2 = createGaussianOverlay(OPT.scale, OPT.scale, 2000, 10)
+    --local overlay3 = createGaussianOverlay(OPT.scale, OPT.scale, 10000, 4)
+    local overlay1 = getGaussianOverlay(10)
+    local overlay2 = getGaussianOverlay(10)
+    local overlay3 = getGaussianOverlay(4)
+    
+    for i=1,N do
+        local img = torch.Tensor(IMG_DIMENSIONS[1], IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
+        local offsetY = math.random(IMG_DIMENSIONS[2])
+        local offsetX = math.random(IMG_DIMENSIONS[3])
+        local baseVal = {math.random(), math.random(), math.random()}
+        
+        for y=1,OPT.scale do
+            for x=1,OPT.scale do
+                for c=1,IMG_DIMENSIONS[1] do
+                    local coords = withinImageCoords(y + c*offsetY, x + c*offsetX, IMG_DIMENSIONS[2], IMG_DIMENSIONS[3])
+                    img[c][y][x] = baseVal[c] + (overlay1[y][x] * overlay2[coords[1]][coords[2]]) + overlay3[coords[1]][coords[2]]
                 end
             end
         end
         
-        table.insert(images, image)
+        img:div(torch.max(img))
+        table.insert(images, img)
     end
     
     return images
+end
+
+function getGaussianOverlay(blurSize)
+    if blurSize == nil then blurSize = 4 end
+    
+    if OVERLAYS == nil then
+        OVERLAYS = {}
+        for i=1,1000 do
+            OVERLAYS[i] = createGaussianOverlay(IMG_DIMENSIONS[2], IMG_DIMENSIONS[3], 10000, 0)
+        end
+    end
+    
+    local overlay1 = OVERLAYS[math.random(#OVERLAYS)]
+    local overlay2 = OVERLAYS[math.random(#OVERLAYS)]
+    local overlay3 = OVERLAYS[math.random(#OVERLAYS)]
+    local overlay4 = OVERLAYS[math.random(#OVERLAYS)]
+    local overlayResult = overlay1:mul(2) - overlay2
+    overlayResult = torch.clamp(overlayResult, 0.0, 1.0)
+    overlayResult = overlayResult + overlay3:cmul(overlay4):mul(2)
+    overlayResult = torch.clamp(overlayResult, 0.0, 1.0)
+    
+    if blurSize > 0 then
+        overlayResult = image.convolve(overlayResult, image.gaussian(blurSize), "same")
+        overlayResult:div(torch.max(overlayResult))
+    end
+    
+    --image.display(overlayResult)
+    --io.read()
+    
+    return overlayResult
+end
+
+function createGaussianOverlay(ySize, xSize, N_points, blurSize)
+    N_points = N_points or 1000
+    blurSize = blurSize or 6
+    local minY = 1
+    local maxY = ySize
+    local minX = 1
+    local maxX = xSize
+    
+    local overlay = torch.Tensor(maxY, maxX)
+    overlay:zero()
+    
+    local directions = {
+        {-1, 0},
+        {-1, 1},
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {1, -1},
+        {0, -1},
+        {-1, -1}
+    }
+    
+    local currentY = math.random(ySize)
+    local currentX = math.random(xSize)
+    local lastY = math.random(ySize)
+    local lastX = math.random(xSize)
+    
+    for i=1,N_points do
+        --print(i)
+        local p = math.random()
+        if p < 0.02 then
+            lastY = currentY
+            lastX = currentX
+            currentY = math.random(maxY)
+            currentX = math.random(maxX)
+        elseif math.random() < 0.10 then
+            currentY = lastY
+            currentX = lastX
+        else
+            lastY = currentY
+            lastX = currentX
+            
+            local found = false
+            while not found do
+                local direction = directions[math.random(#directions)]
+                currentY = lastY + direction[1]
+                currentX = lastX + direction[2]
+                
+                if (currentY >= minY and currentY <= maxY) and (currentX >= minX and currentX <= maxX) then
+                    found = true
+                end
+            end
+        end
+        
+        --print(currentY, currentX, overlay:size())
+        overlay[currentY][currentX] = overlay[currentY][currentX] + 1
+    end
+    
+    overlay:div(torch.max(overlay))
+    if blurSize > 0 then
+        overlay = image.convolve(overlay, image.gaussian(blurSize), "same")
+        overlay:div(torch.max(overlay))
+    end
+    
+    return overlay
+end
+
+function createPixelwiseOverlay(ySize, xSize)
+    local overlay = torch.Tensor(ySize, xSize)
+    overlay:zero()
+    
+    local p = math.random()
+    local pChange = math.random() / 10
+    
+    for y=1,ySize do
+        for x=1,xSize do
+            if math.random() > p then
+                overlay[y][x] = math.min(2*math.random(), 1)
+            else
+                overlay[y][x] = 0
+            end
+            
+            if math.random() > 0.5 then
+                p = math.max(p - pChange, 0)
+            else
+                p = math.min(p + pChange, 1.0)
+            end
+        end
+    end
+    
+    return overlay
+end
+
+function minmax(min_x, x, max_x)
+    if x < min_x then
+        return min_x
+    elseif x > max_x then
+        return max_x
+    else
+        return x
+    end
 end
 
 main()
