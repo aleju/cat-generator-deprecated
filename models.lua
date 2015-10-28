88949eb7a24a1fcc8a3204022e0ebbf59832674e
aleju <kontakt@ajung.name>
2015-10-28 23:23:05 +0100

    Add D with spatial transformer

--- models.lua
+++ models.lua
@@ -2,6 +2,7 @@ require 'torch'
 require 'nn'
 require 'LeakyReLU'
 require 'dpnn'
+require 'layers.cudnnSpatialConvolutionUpsample'
 
 local models = {}
 
@@ -41,18 +42,34 @@ end
 -- @param noiseDim Size of the hidden layer between encoder and decoder.
 -- @returns nn.Sequential
 function models.create_G_decoder(dimensions, noiseDim)
-    local model = nn.Sequential()
+    local inputSz = dimensions[1] * dimensions[2] * dimensions[3]
     local activation = nn.PReLU
   
-    model = nn.Sequential()
+    local model = nn.Sequential()
     --model:add(nn.Linear(noiseDim, 512))
     --model:add(activation())
     --model:add(nn.Linear(512, 2048))
     model:add(nn.Linear(noiseDim, 2048))
     model:add(activation())
-    model:add(nn.Linear(2048, INPUT_SZ))
+    model:add(nn.Linear(2048, inputSz))
     model:add(nn.Sigmoid())
+    --model:add(nn.PReLU())
+    --model:add(nn.Linear(inputSz, inputSz))
     model:add(nn.View(dimensions[1], dimensions[2], dimensions[3]))
+
+    --[[
+    local model = nn.Sequential()
+    model:add(nn.Linear(noiseDim, 512))
+    model:add(activation())
+    model:add(nn.Dropout(0.5))
+    model:add(nn.Linear(512, 2048))
+    model:add(activation())
+    model:add(nn.Dropout(0.25))
+    model:add(nn.Linear(2048, inputSz))
+    model:add(nn.Tanh())
+    model:add(nn.Linear(inputSz, inputSz))
+    model:add(nn.View(dimensions[1], dimensions[2], dimensions[3]))
+    --]]
 
     model = require('weight-init')(model, 'heuristic')
 
@@ -235,6 +252,55 @@ function models.create_D(dimensions)
     conv:add(nn.Dropout())
     conv:add(nn.Linear(1024, 1))
     conv:add(nn.Sigmoid())
+
+    conv = require('weight-init')(conv, 'heuristic')
+
+    return conv
+end
+
+function models.create_D_st(dimensions, cuda)
+    local conv = nn.Sequential()
+    if cuda then
+        conv:add(nn.Copy('torch.FloatTensor', 'torch.CudaTensor', true, true))
+    end
+    conv:add(models.createSpatialTransformer(true, false, false, dimensions[2], dimensions[1], cuda))
+    conv:add(nn.SpatialConvolution(dimensions[1], 64, 3, 3, 1, 1, (3-1)/2))
+    conv:add(nn.PReLU())
+    conv:add(nn.SpatialConvolution(64, 64, 3, 3, 1, 1, (3-1)/2))
+    conv:add(nn.PReLU())
+    
+    local branch1 = nn.Sequential()
+    branch1:add(models.createSpatialTransformer(false, true, true, dimensions[2], 64, cuda))
+    branch1:add(nn.SpatialConvolution(64, 256, 3, 3, 1, 1, (3-1)/2))
+    branch1:add(nn.PReLU())
+    branch1:add(nn.SpatialConvolution(256, 256, 3, 3, 1, 1, (3-1)/2))
+    branch1:add(nn.PReLU())
+    branch1:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+    
+    local branch2 = nn.Sequential()
+    branch2:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+    branch2:add(nn.SpatialConvolution(64, 128, 5, 5, 1, 1, (5-1)/2))
+    branch2:add(nn.PReLU())
+    branch2:add(nn.SpatialConvolution(128, 128, 7, 7, 1, 1, (7-1)/2))
+    branch2:add(nn.PReLU())
+    
+    local concy = nn.Concat(2)
+    concy:add(branch1)
+    concy:add(branch2)
+    
+    conv:add(concy)
+    conv:add(nn.SpatialDropout())
+    conv:add(nn.View((256+128) * 0.25 * dimensions[2] * dimensions[3]))
+    conv:add(nn.Linear((256+128) * 0.25 * dimensions[2] * dimensions[3], 1024))
+    conv:add(nn.PReLU())
+    conv:add(nn.Dropout())
+    conv:add(nn.Linear(1024, 1))
+    conv:add(nn.Sigmoid())
+
+    if cuda then
+        conv:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor', true, true))
+        conv:cuda()
+    end
 
     conv = require('weight-init')(conv, 'heuristic')
 
@@ -435,6 +501,107 @@ function models.create_V(dimensions)
     model = require('weight-init')(model, 'heuristic')
   
     return model
+end
+
+
+--
+-- 
+function models.createSpatialTransformer(allow_rotation, allow_scaling, allow_translation, input_size, input_channels, cuda)
+    if cuda == nil then
+        cuda = true
+    end
+    
+    require 'stn'
+
+    -- Get number of params and initial state
+    local init_bias = {}
+    local nbr_params = 0
+    if allow_rotation then
+        nbr_params = nbr_params + 1
+        init_bias[nbr_params] = 0
+    end
+    if allow_scaling then
+        nbr_params = nbr_params + 1
+        init_bias[nbr_params] = 1
+    end
+    if allow_translation then
+        nbr_params = nbr_params + 2
+        init_bias[nbr_params-1] = 0
+        init_bias[nbr_params] = 0
+    end
+    if nbr_params == 0 then
+        -- fully parametrized case
+        nbr_params = 6
+        init_bias = {1,0,0,0,1,0}
+    end
+
+    -- Create localization network
+    local net = nn.Sequential()
+    net:add(nn.SpatialAveragePooling(2, 2, 2, 2))
+    net:add(nn.SpatialConvolution(input_channels, 16, 3, 3, 1, 1, (3-1)/2))
+    net:add(nn.LeakyReLU())
+    net:add(nn.SpatialConvolution(16, 16, 3, 3, 1, 1, (3-1)/2))
+    net:add(nn.LeakyReLU())
+    net:add(nn.SpatialAveragePooling(2, 2, 2, 2))
+
+    local newHeight = input_size * 0.5 * 0.5
+    net:add(nn.View(16 * newHeight * newHeight))
+    net:add(nn.Linear(16 * newHeight * newHeight, 64))
+    net:add(nn.LeakyReLU())
+    local classifier = nn.Linear(64, nbr_params)
+    net:add(classifier)
+    
+    net = require('weight-init')(net, 'heuristic')
+    -- Initialize the localization network (see paper, A.3 section)
+    classifier.weight:zero()
+    classifier.bias = torch.Tensor(init_bias)
+    
+    local localization_network = net
+
+    -- Create the actual module structure
+    -- branch1 is basically an identity matrix
+    -- branch2 estimates the necessary rotation/scaling/translation (above localization network)
+    -- They both feed into the BilinearSampler, which transforms the image
+    local ct = nn.ConcatTable()
+    local branch1 = nn.Sequential()
+    branch1:add(nn.Transpose({3,4},{2,4}))
+    -- see (1) below
+    if cuda then
+        branch1:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor', true, true))
+    end
+    local branch2 = nn.Sequential()
+    branch2:add(localization_network)
+    branch2:add(nn.AffineTransformMatrixGenerator(allow_rotation, allow_scaling, allow_translation))
+    branch2:add(nn.AffineGridGeneratorBHWD(input_size, input_size))
+    -- see (1) below
+    if cuda then
+        branch2:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor', true, true))
+    end
+    ct:add(branch1)
+    ct:add(branch2)
+
+    local st = nn.Sequential()
+    st:add(ct)
+    local sampler = nn.BilinearSamplerBHWD()
+    -- (1)
+    -- The sampler lead to non-reproducible results on GPU
+    -- We want to always keep it on CPU
+    -- This does no lead to slowdown of the training
+    if cuda then
+        sampler:type('torch.FloatTensor')
+        -- make sure it will not go back to the GPU when we call
+        -- ":cuda()" on the network later
+        sampler.type = function(type) return self end
+        st:add(sampler)
+        st:add(nn.Copy('torch.FloatTensor','torch.CudaTensor', true, true))
+    else
+        st:add(sampler)
+    end
+    --st:add(nn.Copy('torch.CudaTensor','torch.FloatTensor', true, true))
+    st:add(nn.Transpose({2,4},{3,4}))
+    --st:add(nn.Copy('torch.FloatTensor','torch.CudaTensor', true, true))
+
+    return st
 end
 
 return models
