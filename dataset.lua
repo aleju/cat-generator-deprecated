7477ddb39ad968ce6b5859516d5143ead43e1bce
aleju <kontakt@ajung.name>
2015-11-03 22:08:35 +0100

    Add method to load images non randomized, add normalization

--- dataset.lua
+++ dataset.lua
@@ -67,9 +67,42 @@ function dataset.loadPaths()
         end
     end
     
-    print(string.format("<dataset> Loaded %d filepaths", #files))
+    --print(string.format("<dataset> Loaded %d filepaths", #files))
     
     dataset.paths = files
+end
+
+-- Load images from the dataset.
+-- @param startAt Number of the first image.
+-- @param count Count of the images to load.
+-- @return Table of images. You can call :size() on that table to get the number of loaded images.
+function dataset.loadImages(startAt, count)
+    local endBefore = startAt + count
+    if dataset.paths == nil then
+        dataset.loadPaths()
+    end
+
+    local N = math.min(count, #dataset.paths)
+    local images = torch.FloatTensor(N, dataset.nbChannels, dataset.scale, dataset.scale)
+    for i=1,N do
+        local img = image.load(dataset.paths[i], dataset.nbChannels, "float")
+        img = image.scale(img, dataset.scale, dataset.scale)
+        images[i] = img
+    end
+
+    local result = {}
+    result.data = images
+    
+    function result:size()
+        return N
+    end
+
+    setmetatable(result, {
+        __index = function(self, index) return self.data[index] end,
+        __len = function(self) return self.data:size(1) end
+    })
+
+    return result
 end
 
 -- Loads a defined number of randomly selected images from
@@ -82,6 +115,8 @@ function dataset.loadRandomImages(count)
     local data = torch.FloatTensor(#images, dataset.nbChannels, dataset.scale, dataset.scale)
     for i=1, #images do
         data[i] = image.scale(images[i], dataset.scale, dataset.scale)
+        --data[i] = image.rotate(data[i], (math.random()*math.pi/4) - (math.pi/8), "bilinear")
+        --data[i] = image.rotate(data[i], math.pi/4, "bilinear")
     end
 
     --local ker = torch.ones(3)
@@ -96,11 +131,17 @@ function dataset.loadRandomImages(count)
         return N
     end
 
-    setmetatable(result, {__index = function(self, index)
-        return self.scaled[index]
-    end})
+    function result:normalize(mean, std)
+        mean, std = NN_UTILS.normalize(result.scaled, mean, std)
+        return mean, std
+    end
 
-    print(string.format('<dataset> loaded %d random examples', N))
+    setmetatable(result, {
+        __index = function(self, index) return self.scaled[index] end,
+        __len = function(self) return self.scaled:size(1) end
+    })
+    
+    --print(string.format('<dataset> loaded %d random examples', N))
 
     return result
 end
